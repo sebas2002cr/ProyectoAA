5e9a6b1035c45be8caa1f60bc2c2b8312b7816e5
Sebastian Obando Paniagua <60985278+sebas2002cr@users.noreply.github.com>
2021-04-28 18:49:16 -0600

    Algoritmo de procesamiento de imagenes con OpenCv

---
+++ ImageProcessing.cpp
@@ -0,0 +1,26 @@
+// ImageProcessing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+int main()
+{
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+    if (img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    if (k == 's')
+    {
+        imwrite("starry_night.png", img);
+    }
+    return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu

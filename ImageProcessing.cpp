ea6122c4326cf61bc98d83211093c94a72746873
Sebastian Obando Paniagua <60985278+sebas2002cr@users.noreply.github.com>
2021-04-29 19:40:20 -0600

    UPDATE - Operaciones con imágenes en OpenCV (Matices , variaciones de colores y lecturas)

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -22,5 +22,89 @@ int main()
     }
     return 0;
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+
+
+// -----------------------------------------OPERACIONES CON IMAGENES--------------------------------------------------------------------------------
+
+//
+//Load an image from a file:
+Mat img = imread(filename);
+
+//
+//grayscale
+Mat img = imread(filename, IMREAD_GRAYSCALE);
+
+
+//
+//grayscale
+Mat img = imread(filename, IMREAD_GRAYSCALE);
+
+
+
+
+
+//
+//  Accessing pixel intensity values
+//  3 channel image with BGR color ordering (the default format returned by imread):
+//
+ Vec3b intensity = img.at<Vec3b>(y, x);
+            uchar blue = intensity.val[0];
+            uchar green = intensity.val[1];
+            uchar red = intensity.val[2];
+
+// access a point in this matrix using the same method Mat::at
+ Point2f point = pointsMat.at<Point2f>(i, 0);
+
+
+//There is a number of convenient operators defined on a matrix. For example, here is how we can make a black image from an existing greyscale image img
+
+            img = Scalar(0);
+//Selecting a region of interest:
+
+            Rect r(10, 10, 100, 100);
+            Mat smallImg = img(r);
+//A conversion from Mat to C API data structures (C++ only):
+
+        Mat img = imread("image.jpg");
+        IplImage img1 = cvIplImage(img);
+        CvMat m = cvMat(img);
+//Note that there is no data copying here.
+
+//Conversion from color to greyscale:
+
+        Mat img = imread("image.jpg"); // loading a 8UC3 image
+        Mat grey;
+        cvtColor(img, grey, COLOR_BGR2GRAY);
+//Change image type from 8UC1 to 32FC1:
+
+        src.convertTo(dst, CV_32F);
+
+
+//It is very useful to see intermediate results of your algorithm during development process.
+//OpenCV provides a convenient way of visualizing images. A 8U image can be shown using:
+
+        Mat img = imread("image.jpg");
+        namedWindow("image", WINDOW_AUTOSIZE);
+        imshow("image", img);
+        waitKey();
+//A call to waitKey() starts a message passing cycle that waits for a key stroke in the "image" window. 
+//A 32F image needs to be converted to 8U type. For example:
+
+        Mat img = imread("image.jpg");
+        Mat grey;
+        cvtColor(img, grey, COLOR_BGR2GRAY);
+        Mat sobelx;
+        Sobel(grey, sobelx, CV_32F, 1, 0);
+        double minVal, maxVal;
+        minMaxLoc(sobelx, &minVal, &maxVal); //find minimum and maximum intensities
+        Mat draw;
+        sobelx.convertTo(draw, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
+        namedWindow("image", WINDOW_AUTOSIZE);
+        imshow("image", draw);
+        waitKey();
+
+
+
+//Codigo de: https://docs.opencv.org/3.4/d5/d98/tutorial_mat_operations.html
+
+// -----------------------------------------OPERACIONES CON IMAGENES--------------------------------------------------------------------------------

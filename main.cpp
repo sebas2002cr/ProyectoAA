5aacd1b53ba1baf7e0f37d65e0f583408d261b0a
Fabioncho0514 <fabicran@gmail.com>
2021-05-01 08:45:32 -0600

    agrego codigo leer imagen

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
-#include <iostream>
-#include "funcionesAuxiliares.h"
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-using namespace std;
+using namespace cv;
 
-
-int main() 
-{ 
-    int arr[] = {12,23,3,43,51,35,19,45}; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-    cout<<"Input array"<<endl;
-    displayArray(arr,n);
-    cout<<endl;
-    quickSort(arr, 0, n-1); 
-    cout<<"Array sorted with quick sort"<<endl; 
-    displayArray(arr,n); 
-    return 0; 
+int main(){
+    Mat img=imread("C:\\Users\\fabic\\Desktop\\ProyectoAA\\img\\tiger.png");
+    imshow("IMAGE", img);
+    waitKey(0);
+    return 0;
 }
